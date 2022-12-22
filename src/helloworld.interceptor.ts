a9382a8b9eb78314883f232b26adbbb3bc25224b
judyjose008 <judy@elaachi.com>
2022-12-22 18:51:41 +0530

    Interceptor added

--- src/helloworld.interceptor.ts
+++ src/helloworld.interceptor.ts
@@ -1,13 +1,17 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
+
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class HelloWorldInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // This console.log() will work before entering the contoller
     console.log("Before entering the contoller")
-    // This code will run after the code executes logic from contoller
-    return next.handle().pipe( tap(() => console.log(`Hello World!`)));
-  }
+    
+    return next.handle().pipe( tap(() => 
+        // This console.log() will run after the code executes logic from contoller
+        console.log(`Hello World!`)
+    ));
+}
 }
